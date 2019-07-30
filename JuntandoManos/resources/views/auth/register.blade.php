53c2aa0ebab473170014b1ee6ca13d73b8af1096
matiramone7 <mati.leo@outlook.com>
2019-07-29 21:38:17 -0300

    Bla

--- JuntandoManos/resources/views/auth/register.blade.php
+++ JuntandoManos/resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('front.template')
 
-@section('content')
+@section('mainContent')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">

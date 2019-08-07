69d3b78014a389d1a0e36a2417a5592f690b9fa9
agustinnadalich <agustin.nadalich@gmail.com>
2019-08-07 16:09:20 -0300

    arreglado provincias en edit user profile

--- JuntandoManos/resources/views/front/user/profile.blade.php
+++ JuntandoManos/resources/views/front/user/profile.blade.php
@@ -124,7 +124,30 @@
                                     <div class="col-md-6">
                                       <select id="province" name="province" class="form-control">
                                         <option value="{{Auth::user()->province}}" selected >{{Auth::user()->province}}</option>
-                                        <option value="vjbafsjaon">jaksbfiaj</option>
+                                        <option value="Misiones">Misiones</option>
+                                        <option value="San Luis">San Luis</option>
+                                        <option value="San Juan">San Juan</option>
+                                        <option value="Entre Ríos">Entre Ríos</option>
+                                        <option value="Santa Cruz">Santa Cruz</option>
+                                        <option value="Río Negro">Río Negro</option>
+                                        <option value="Chubut">Chubut</option>
+                                        <option value="Córdoba">Córdoba</option>
+                                        <option value="Mendoza">Mendoza</option>
+                                        <option value="La Rioja">La Rioja</option>
+                                        <option value="Catamarca">Catamarca</option>
+                                        <option value="La Pampa">La Pampa</option>
+                                        <option value="Santiago del Estero">Santiago del Estero</option>
+                                        <option value="Corrientes">Corrientes</option>
+                                        <option value="Santa Fe">Santa Fe</option>
+                                        <option value="Tucumán">Tucumán</option>
+                                        <option value="Neuquén">Neuquén</option>
+                                        <option value="Salta">Salta</option>
+                                        <option value="Chaco">Chaco</option>
+                                        <option value="Formosa">Formosa</option>
+                                        <option value="Jujuy">Jujuy</option>
+                                        <option value="Ciudad Autónoma de Buenos Aires">Ciudad Autónoma de Buenos Aires</option>
+                                        <option value="Buenos Aires">Buenos Aires</option>
+                                        <option value="Tierra del Fuego, Antártida e Islas del Atlántico Sur">Tierra del Fuego, Antártida e Islas del Atlántico Sur</option>
                                       </select>
                                   @else
                                     <div class="form-group row" style="display: none;">

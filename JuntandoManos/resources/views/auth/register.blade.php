01598fbf9845b9ac9fc98e9791237242309f289d
cecitarsia <ceciliatarsia@gmail.com>
2019-08-07 10:36:21 -0300

    botones en login y register

--- JuntandoManos/resources/views/auth/register.blade.php
+++ JuntandoManos/resources/views/auth/register.blade.php
@@ -145,7 +145,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn a_btn">
                                     {{ __('Register') }}
                                 </button>
                             </div>
